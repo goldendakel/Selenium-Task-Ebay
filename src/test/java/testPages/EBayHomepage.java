e570cd102dde5b6ca16be94ea3e393a540c1485a
goldendakel <tsvetomirhadzhiev@gmail.com>
2024-04-02 11:21:36 +0300

    Finilazied version of the framework

--- src/test/java/testPages/EBayHomepage.java
+++ src/test/java/testPages/EBayHomepage.java
@@ -7,13 +7,14 @@ import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import io.cucumber.java.en.Given;
 
 public class EBayHomepage extends SeleniumHelper {
 
     @Given("the user is on eBay homepage {string}")
     public void the_user_is_on_eBay_homepage(String userKey) {
+
+        //Navigate to https://ebay.com/
         try {
             JSONParser parser = new JSONParser();
             Object obj = parser.parse(new FileReader("src/test/resources/componentsJson/EBayURLs.json"));
@@ -25,12 +26,15 @@ public class EBayHomepage extends SeleniumHelper {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        //Verify that the page is correct and opened
         String expectedUrl = "https://www.ebay.com/";
         String actualUrl = getDriver().getCurrentUrl();
         logger.info("actual URL is: {}", actualUrl);
         logger.info("expected URL is: {}", expectedUrl);
         assertEquals(actualUrl, expectedUrl, "Page URL doesn't match expected URL");
     }
+
 
     @Given("the user accepts all cookies")
     public void the_user_accepts_all_cookies() {
--- src/test/resources/features/sandbox/sandbox.feature
+++ src/test/resources/features/sandbox/sandbox.feature
@@ -2,13 +2,16 @@
 Feature: Testing separate scenarios under development
 
 
-  Scenario: eBay Test
+  Scenario: Adding a product to the cart
     Given the user is on eBay homepage "eBay_Homepage"
     And the user accepts all cookies
     When the user navigates to the 'Toys' category
     And the user searches for 'Monopoly'
     And the user selects one of the Monopoly Products
     And the user navigates to the 'Shipping, returns, and payments' tab on PDP
+    And the user updates the quantity of the product on PDP
     And the user adds the product to Cart
+    Then the user verifies the quantity of the product on Cart page "eBay_Cartpage"
+
 
     
